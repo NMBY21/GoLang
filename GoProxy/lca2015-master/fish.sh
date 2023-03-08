fdf253241f6794643695c43771cf11d6b3412a58
NMBY21 <betelihemmengist21@gmail.com>
2023-03-08 11:38:19 +0300

    go

--- GoProxy/lca2015-master/fish.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/fish
-
-set -xg PATH (pwd)/go/bin $PATH
-set -xg GOROOT (pwd)/go
-set -xg GOPATH (pwd)/gopath
-mkdir -p $GOPATH
--- GoProxy/lca2015-master/zsh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export PATH="$(pwd)/go/bin:$PATH"
-export GOROOT="$(pwd)/go"
-export GOPATH="$(pwd)/gopath"
-mkdir -p $GOPATH
